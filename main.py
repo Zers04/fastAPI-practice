8d08246ef59ecd9603b502b445b513ce6599dfa2
Valti <valentinalondono911@gmail.com>
2025-04-07 19:31:32 -0500

    Create Get, Put and Delete

--- main.py
+++ main.py
@@ -65,4 +65,74 @@ async def create_animal(animal: animalBase, db: db_depndency):
     db.refresh(db_animal)
     return db_animal
 
+@app.get("/animales/", response_model=List[animalBase])
+async def read_animals(db: db_depndency):
 
+    # Get all animals from the database
+    animals = db.query(models.Animals).all()
+
+    # Check if there are any animals in the database
+    if not animals:
+        return {"message": "No animals found"}
+
+    return animals
+
+
+@app.get("/animales/{animal_id}", response_model=animalBase)
+async def read_animal(animal_id: int, db: db_depndency):
+    # Get a specific animal by ID from the database
+    animal = db.query(models.Animals).filter(models.Animals.id == animal_id).first()
+
+    # Check if the animal exists in the database
+    if animal is None:
+        raise HTTPException(status_code=404, detail="Animal not found")
+    
+    return animal
+
+
+@app.put("/animales/{animal_id}", response_model=animalBase)
+async def update_animal(animal_id: int, animal: animalBase, db: db_depndency):
+    
+    # Check if the species is valid
+    species = [e.value for e in models.EspecieEnum]
+    if animal.especie not in species:
+        raise HTTPException(status_code=400, detail="Invalid species, must be 'perro' or 'gato'")
+    
+    # Check if the breed is valid
+    breeds = [e.value for e in models.RazaEnum]
+    if animal.raza not in breeds:
+        raise HTTPException(status_code=400, detail="Invalid breed, must be 'criollo' or 'fino'")
+    
+    # Check if the age and weight are valid
+    if animal.edad <= 0:
+        raise HTTPException(status_code=400, detail="Invalid age")
+    
+    if animal.peso <= 0:
+        raise HTTPException(status_code=400, detail="Invalid weight")
+
+    # Check if the animal exists in the database
+    db_animal = db.query(models.Animals).filter(models.Animals.id == animal_id).first()
+
+    if db_animal is None:
+        raise HTTPException(status_code=404, detail="Animal not found")
+    
+    # Update the animal's attributes
+    db_animal.especie = animal.especie
+    db_animal.raza = animal.raza
+    db_animal.nombre = animal.nombre
+    db_animal.edad = animal.edad
+    db_animal.peso = animal.peso
+
+    # Add the updated animal to the database
+    db.commit()
+    db.refresh(db_animal)
+    return db_animal
+
+@app.delete("/animales/{animal_id}")
+async def delete_animal(animal_id: int, db: db_depndency):
+    db_animal = db.query(models.Animals).filter(models.Animals.id == animal_id).first()
+    if db_animal is None:
+        raise HTTPException(status_code=404, detail="Animal not found")
+    db.delete(db_animal)
+    db.commit()
+    return {"detail": "Animal deleted"}
